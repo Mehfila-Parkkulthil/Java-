0944c8c63edeb1e6cc33706d57cba6b09f626749
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-02 20:10:44 +0530

    modified files

---
+++ Question52.java
@@ -0,0 +1,29 @@
+//Question 52
+//Write a function that calculates the factorial of a given number.
+
+import java.util.Scanner;
+public class Question52{
+    public static void main(String[] args){
+        System.out.println("Hi , Lets try another way to do factorial");
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a=scanner.nextInt();
+
+    }
+
+    public static void factorial(int a){
+     
+        if(a<2){
+            return 1;
+        }
+         
+        long fact =1;
+        int i=2;
+        while (i<=a) {
+            fact =fact*i;
+            i++;  
+        }
+
+
+    }
+}
