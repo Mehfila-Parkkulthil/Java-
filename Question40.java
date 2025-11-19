154b621de0d62321aa4c5e2108fe70dec9781f80
Mehfila Parkkulthil <mehfilaparkkulthil@gmail.com>
2025-11-19 16:15:52 +0530

    Update Question40.java

--- Question40.java
+++ Question40.java
@@ -1,2 +1,9 @@
 //Question 40
 //Create a progrm to reverse the digits of a number
+
+import java.util.Scanner;
+public class Question40{
+  public static void main(String[]args){
+    println("Lets reverse the digits");
+    
+  }
